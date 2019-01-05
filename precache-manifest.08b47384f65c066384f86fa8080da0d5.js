c4bb5124f320f2b6c1259a3970591ef240e29f90
Maxime <hardymaxime7@gmail.com>
2019-01-05 17:52:20 +0100

    Updates

---
+++ precache-manifest.08b47384f65c066384f86fa8080da0d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc0e4e5a0f45363cf778ad5b205b237a",
+    "url": "/react-carousel/static/media/right.fc0e4e5a.svg"
+  },
+  {
+    "revision": "d5c29f9fbbf8d35ae6db8740e0f57b7a",
+    "url": "/react-carousel/static/media/left.d5c29f9f.svg"
+  },
+  {
+    "revision": "fc4d8574e6ea008984bd",
+    "url": "/react-carousel/static/js/runtime~main.fc4d8574.js"
+  },
+  {
+    "revision": "93d9a663803619a95d9e",
+    "url": "/react-carousel/static/js/main.93d9a663.chunk.js"
+  },
+  {
+    "revision": "fed304020308db9f0dbc",
+    "url": "/react-carousel/static/js/1.fed30402.chunk.js"
+  },
+  {
+    "revision": "93d9a663803619a95d9e",
+    "url": "/react-carousel/static/css/main.52ef6038.chunk.css"
+  },
+  {
+    "revision": "580331c5ae5f1693748935b3137968ea",
+    "url": "/react-carousel/index.html"
+  }
+];
